465d9c6e0bc5e731e5e9ffd502c741e169be13d6
binkybear <binkybear@nethunter.com>
2014-12-11 18:53:58 -0600

    Revert "Permissive selinux policy"
    This reverts commit 0725321de59dc3efb5eff32bdc4343bc08fa732e.

--- ramdisk/soure/sbin/lk-post-boot.sh
+++
@@ -1,8 +0,0 @@
-#!/system/bin/sh
-
-# for lkconfig
-[ ! -d "/data/data/leankernel" ] && mkdir /data/data/leankernel
-chmod 755 /data/data/leankernel
-
-# init.d support
-/system/xbin/busybox run-parts /system/etc/init.d
